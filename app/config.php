3cfe395ee11c9bfeadff733742c25c30b62605c1
546669204 <546669204@qq.com>
2018-03-19 11:15:58 +0800

    去除默认调试

--- app/config.php
+++ app/config.php
@@ -17,9 +17,9 @@ return [
     // 应用命名空间
     'app_namespace'          => 'app',
     // 应用调试模式
-    'app_debug'              => true,
+    'app_debug'              => false,
     // 应用Trace
-    'app_trace'              => true,
+    'app_trace'              => false,
     // 应用模式状态
     'app_status'             => '',
     // 是否支持多模块
